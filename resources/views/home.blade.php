34b3fe8f39be7798c3178da8246f8d03bf2baebe
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-07 15:15:21 +0100

    Crete Hello Word Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <main>
+        <h1>Ciao Mondo</h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet amet cum doloremque quasi aliquid assumenda eligendi minima, tempore nesciunt in corrupti cupiditate ipsum natus, laudantium maxime possimus dolor. Ipsum, numquam.
+        </p>
+        <img src="https://www.zend.com/sites/default/files/image/2019-09/logo-laravel.jpg" alt="Laravel Logo">
+    </main>
+</body>
+
+</html>
